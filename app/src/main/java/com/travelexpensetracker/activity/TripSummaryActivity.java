5187d399c2785de5c75638654167351598cda36a
Brijesh Dutta <brijeshdtt@gmail.com>
2017-09-01 20:47:03 +0530

    Version 1.0.0.0 - added expesnse summary

--- app/src/main/java/com/travelexpensetracker/activity/TripSummaryActivity.java
+++ app/src/main/java/com/travelexpensetracker/activity/TripSummaryActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,8 +18,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 import com.travelexpensetracker.R;
+import com.travelexpensetracker.adapter.PersonListAdapter;
 import com.travelexpensetracker.database.DatabaseValues;
+import com.travelexpensetracker.model.Person;
 import com.travelexpensetracker.model.Trip;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TripSummaryActivity extends AppCompatActivity {
 
@@ -26,6 +33,10 @@ public class TripSummaryActivity extends AppCompatActivity {
     String sTripId,sTripName,sTripDate;
 
     TextView tvDisplayTripName,tvDisplayTripDate;
+    ListView listViewTripPersons;
+    CardView cardViewExpenseSummary;
+
+    List<Person> personList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +48,7 @@ public class TripSummaryActivity extends AppCompatActivity {
         getTripDetails();
         initializeUiCompoenets();
         getTripDetailsFromDatabase();
+        loadPersonDetailsFromDb();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,13 +59,53 @@ public class TripSummaryActivity extends AppCompatActivity {
         });
     }
 
+    private void loadPersonDetailsFromDb() {
+        final DatabaseReference databaseReferencePersonDetails = DatabaseValues.getPersonDetailsReference();
+        databaseReferencePersonDetails.keepSynced(true);
+        final PersonListAdapter personListAdapter = new PersonListAdapter(TripSummaryActivity.this,personList);
+        databaseReferencePersonDetails.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()){
+                    personList.clear();
+                    for (DataSnapshot  personDetailSnapshot : dataSnapshot.getChildren()){
+                        Person person = personDetailSnapshot.getValue(Person.class);
+                        if (person.getsTripId().equalsIgnoreCase(sTripId)){
+                            personList.add(0,person);
+                            personListAdapter.notifyDataSetChanged();
+                        }
+                    }
+                    listViewTripPersons.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            listViewTripPersons.smoothScrollToPosition(0);
+                        }
+                    });
+                    listViewTripPersons.setAdapter(personListAdapter);
+                    personListAdapter.notifyDataSetChanged();
+                    cardViewExpenseSummary.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
     private void initializeUiCompoenets() {
         tvDisplayTripName = (TextView) findViewById(R.id.tvDisplayTripName);
         tvDisplayTripDate = (TextView) findViewById(R.id.tvDisplayTripDate);
+        listViewTripPersons = (ListView) findViewById(R.id.listViewTripPersons);
+        cardViewExpenseSummary = (CardView) findViewById(R.id.cardViewExpenseSummary);
+        personList = new ArrayList<>();
     }
 
     private void getTripDetailsFromDatabase() {
         DatabaseReference databaseReferenceTripDetails = DatabaseValues.getTripDetailsReference();
+        databaseReferenceTripDetails.keepSynced(true);
         databaseReferenceTripDetails.orderByChild("sTripId").equalTo(sTripId);
         databaseReferenceTripDetails.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
---
+++ app/src/main/java/com/travelexpensetracker/adapter/PersonListAdapter.java
@@ -0,0 +1,69 @@
+package com.travelexpensetracker.adapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.travelexpensetracker.R;
+import com.travelexpensetracker.model.Person;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Rini Banerjee on 01-09-2017.
+ */
+
+public class PersonListAdapter extends ArrayAdapter<Person> {
+
+    private Activity context;
+    private List<Person> personDetailsList;
+    Person personDetails;
+
+
+    public PersonListAdapter(Activity context,List<Person> expenseDetailsList){
+        super(context, R.layout.card_layout_view_person_from_trip,expenseDetailsList);
+        this.context = context;
+        this.personDetailsList = expenseDetailsList;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater layoutInflater = context.getLayoutInflater();
+        final View listViewItems = layoutInflater.inflate(R.layout.card_layout_view_person_from_trip,null,true);
+
+        TextView tvPersonName = (TextView) listViewItems.findViewById(R.id.tvPersonName);
+        TextView tvPersonMobileNo = (TextView) listViewItems.findViewById(R.id.tvPersonMobileNo);
+
+        personDetails = personDetailsList.get(position);
+
+        tvPersonName.setText(personDetails.getsPersonName());
+        tvPersonMobileNo.setText("("+personDetails.getsPersonMobileNo()+")");
+        return listViewItems;
+    }
+}
